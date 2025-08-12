6c4b8bf2f80341e327fd3de3f81f7d1469e63231
TJ Taylor <dugancathal@gmail.com>
2025-08-11 22:56:31 -0600

    Add lib dir for setup logic
    This has gotten complex enough...

---
+++ setup_lib/dotfiles.rb
@@ -0,0 +1,103 @@
+require "pathname"
+require "fileutils"
+
+module ClassConf
+  def clatter(name, default: nil)
+    ivar_name = "@#{name}"
+    define_singleton_method(name) do
+      return instance_variable_get(ivar_name) if instance_variable_defined?(ivar_name)
+      
+      instance_variable_set(ivar_name, default)
+      default
+    end
+
+    define_singleton_method("#{name}=") do |other|
+      instance_variable_set(ivar_name, other)
+    end
+
+    define_singleton_method("with_#{name}") do |other, &block|
+      original = instance_variable_get(ivar_name)
+      instance_variable_set(ivar_name, other)
+      block.call(other)
+    ensure
+      instance_variable_set(ivar_name, original)
+    end
+  end
+end
+
+module Dotfiles
+  IGNORED_FILES = %w[Rakefile Gemfile Gemfile.lock README.md LICENSE setup_lib]
+
+  extend ClassConf
+
+  clatter :dest, default: Pathname(Dir.home)
+  clatter :source, default: Pathname(File.expand_path("..", __dir__))
+
+  def self.os_name(host_os = RbConfig::CONFIG['host_os'])
+    case host_os
+      when /linux/ then 'linux'
+      when /darwin/ then 'mac'
+      else raise "OS not supported: #{host_os}"
+    end
+  end
+
+  def self.merge_install
+    MergeInstallation.new.call(to: dest, from: source)
+  end
+
+  def self.link_files
+  end
+
+  module Ui
+    def self.console = ConsoleUi.new
+    def self.recording = RecordingUi.new
+
+    class ConsoleUi
+      def initialize(stdout = $stdout, stdin = $stdin)
+        @stdout = $stdout
+        @stdin = $stdin
+        @do_all = false
+      end
+
+      def ask(query)
+        return true if do_all_after_ask?
+        @stdout.print "#{query} [ynaq] "
+
+        case @stdin.gets.chomp
+        when /y/i then true
+        when /n/i then false
+        when /a/i then @do_all = true
+        when /q/i then exit
+        end
+      end
+
+      def do_all_after_ask? = @do_all
+    end
+  end
+
+  class MergeInstallation
+    def initialize(ui = Ui.console)
+      @ui = ui
+    end
+
+    def call(to:, from:, ignore: IGNORED_FILES, dest_file_prefix: ".")
+      from.children.each do |source_file|
+        next if ignore.include?(source_file.basename.to_s)
+
+        dest_file = to.join("#{dest_file_prefix}#{source_file.basename}")
+        next call(to: dest_file, from: source_file, ignore:, dest_file_prefix: "") if dest_file.directory?
+
+        next merge_content(to: dest_file, from: source_file) if dest_file.exist?
+
+        FileUtils.ln_s(source_file, dest_file)
+      end
+    end
+
+    def merge_content(to:, from:, comment_prefix: "#")
+      return if to.read.include?("#{comment_prefix} ===dotfiles===\n")
+      to.open("a") do |f|
+        f.write("\n#{comment_prefix} ===dotfiles===\n\n" + from.read)
+      end
+    end
+  end
+end
---
+++ setup_lib/dotfiles_test.rb
@@ -0,0 +1,152 @@
+require "minitest/autorun"
+require_relative "./dotfiles"
+
+class TestDotfiles < Minitest::Test
+  def test_clatter_getter_setter
+    klass = Class.new do
+      extend ClassConf
+      clatter :attr_a
+    end
+
+    assert_nil klass.attr_a
+
+    klass.attr_a = "foo"
+
+    assert_equal "foo", klass.attr_a
+  end
+
+  def test_clatter_default
+    klass = Class.new do
+      extend ClassConf
+      clatter :attr_a, default: 'foo'
+    end
+
+    assert_equal "foo", klass.attr_a
+  end
+
+  def test_clatter_with_attr
+    klass = Class.new do
+      extend ClassConf
+      clatter :attr_a, default: 'foo'
+    end
+
+    was_executed = false
+    klass.with_attr_a("bar") do
+      was_executed = true
+      assert_equal "bar", klass.attr_a
+    end
+
+    assert was_executed
+  end
+end
+
+class TestDotfiles < Minitest::Test
+  def test_merge_install_copies_dotfiles
+    with_tmp_proj(%w[zshrc zshenv]) do |src, dest|
+      Dotfiles::MergeInstallation.new.call(to: dest, from: src)
+
+      assert_includes children_of(dest), ".zshrc"
+      assert_includes children_of(dest), ".zshenv"
+    end
+  end
+
+  def test_merge_install_appends_when_file_exists
+    with_tmp_proj(%w[zshrc]) do |src, dest|
+      src.join("zshrc").write("new\ncontent\n")
+      dest.join(".zshrc").write("already exists\nwith\nsome\ncontent\n")
+
+      Dotfiles::MergeInstallation.new.call(to: dest, from: src)
+
+      assert_includes children_of(dest), ".zshrc"
+      assert_equal dest.join(".zshrc").read, <<~TXT
+        already exists
+        with
+        some
+        content
+
+        # ===dotfiles===
+
+        new
+        content
+      TXT
+    end
+  end
+
+  def test_merge_install_ignores_pre_merged_files
+    with_tmp_proj(%w[zshrc]) do |src, dest|
+      src.join("zshrc").write("new\ncontent\n")
+      dest.join(".zshrc").write("already exists\n")
+
+      Dotfiles::MergeInstallation.new.call(to: dest, from: src)
+      Dotfiles::MergeInstallation.new.call(to: dest, from: src)
+
+      assert_equal dest.join(".zshrc").read, <<~TXT
+        already exists
+
+        # ===dotfiles===
+
+        new
+        content
+      TXT
+    end
+  end
+
+  def test_merge_install_ignores_specified_files
+    with_tmp_proj(%w[zshrc Gemfile LICENSE]) do |src, dest|
+      Dotfiles::MergeInstallation.new.call(
+        to: dest,
+        from: src,
+        ignore: %w[Gemfile LICENSE]
+      )
+
+      refute_includes children_of(dest), ".Gemfile"
+      refute_includes children_of(dest), ".LICENSE"
+    end
+  end
+
+  def test_merge_install_links_directories
+    with_tmp_proj(zsh: %w[aliases]) do |src, dest|
+      Dotfiles::MergeInstallation.new.call(to: dest, from: src)
+
+      assert_includes children_of(dest), ".zsh"
+      assert_includes children_of(dest.join(".zsh")), "aliases"
+    end
+  end
+
+  def test_merge_install_links_directory_children_when_parent_exists
+    with_tmp_proj(zsh: %w[aliases]) do |src, dest|
+      dest.join(".zsh").mkdir
+      Dotfiles::MergeInstallation.new.call(to: dest, from: src)
+
+      assert_includes children_of(dest), ".zsh"
+      assert_includes children_of(dest.join(".zsh")), "aliases"
+    end
+  end
+
+  private
+
+  def source_path = Pathname(File.expand_path("..", __dir__))
+
+  def children_of(dir) = dir.children.map(&:basename).map(&:to_s)
+
+  def with_tmp_proj(*paths, **dirs)
+    Dir.mktmpdir do |dest_dir|
+      Dir.mktmpdir do |src_dir|
+        src = Pathname(src_dir)
+
+        Array(paths).flatten.each { src.join(_1).write(src) }
+        dirs.each do |dir, children| 
+          nested = src.join(dir.to_s)
+          nested.mkdir
+
+          Array(children).each do |child|
+            nested.join(child).write(src)
+          end
+        end
+
+        yield(src, Pathname(dest_dir))
+      end
+    end
+  end
+end
+
