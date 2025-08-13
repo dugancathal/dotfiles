218bbe69fab679c779fdae139df09d08a07a7b94
TJ Taylor <dugancathal@gmail.com>
2025-08-12 22:31:11 -0600

    Ignore bootstrap file

--- setup_lib/dotfiles.rb
+++ setup_lib/dotfiles.rb
@@ -26,7 +26,7 @@ module ClassConf
 end
 
 module Dotfiles
-  IGNORED_FILES = %w[Rakefile Gemfile Gemfile.lock README.md LICENSE setup_lib .git]
+  IGNORED_FILES = %w[Rakefile Gemfile Gemfile.lock README.md LICENSE setup_lib .git bootstrap.sh]
 
   extend ClassConf
 
