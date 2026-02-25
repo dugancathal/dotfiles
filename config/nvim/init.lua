e750a8a4dfcc0ecb236719c353b88c1ce2c397b9
TJ Taylor <dugancathal@gmail.com>
2026-02-25 12:40:43 -0700

    Add nvim uuid func

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -22,3 +22,10 @@ vim.cmd.colorscheme "catppuccin"
 require("config.fzf")
 require("config.lsp")
 require("plugins.gitcoauthor")
+
+-- vim.keymap.set("n", "<leader>u", "i#ID:<Esc>:read !uuidgen<Enter>kJx:s/-//g<Enter>", { desc = "insert an upcase UUID with no -" })
+local function insert_uuid()
+  local uuid = vim.fn.system('uuidgen'):gsub('%s+', ''):gsub('-', '')
+  vim.api.nvim_put({"ID:" .. uuid}, 'c', true, true)
+end
+vim.keymap.set('n', '<leader>u', insert_uuid, { desc = 'Insert UUID from shell' })
